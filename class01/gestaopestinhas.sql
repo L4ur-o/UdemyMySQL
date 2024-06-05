06de25ced247f1b4a5e70ceaddbe524f41655ee1
bckendtoon <academicolauro@hotmail.com>
2024-06-04 23:04:09 -0300

    primeiro commit

---
+++ class01/gestaopestinhas.sql
@@ -0,0 +1,19 @@
+/* 01 -> Criar um BD:
+	CREATE DATABASE gestaoalunos
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_0900_ai_ci;
+    
+    DROP DATABASE gestaoalunos; -> Para excluir tua BD
+*/
+
+/* 02 -> Criando tabelas:
+CREATE TABLE `gestaoalunos`.`alunos` (
+	`idaluno` INT NOT NULL AUTO_INCREMENT,
+	`nomealuno` VARCHAR(40) NOT NULL,
+	`numaluno` VARCHAR(15) NOT NULL, 
+	`altnumaluno` VARCHAR(15),
+	PRIMARY KEY (`idaluno`));
+    
+	DROP TABLE alunos; -> Excluir tabelas da tua BD
+*/
+
