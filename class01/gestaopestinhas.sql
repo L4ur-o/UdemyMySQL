3f45a54cc6587e4e43f2b35f22ccdb8a367df7ee
bckendtoon <academicolauro@hotmail.com>
2024-06-07 08:20:50 -0300

    mudança

--- class01/gestaopestinhas.sql
+++ class01/gestaopestinhas.sql
@@ -1,22 +1,42 @@
-/* 01 -> Criar um BD:
-	CREATE DATABASE gestaoalunos
+/*
+	CREATE DATABASE 'livraria'
 	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_0900_ai_ci;
-    
+    ------------------------------
     DROP DATABASE gestaoalunos; -> Para excluir tua BD
 */
-
-/* 02 -> Criando tabelas:
-CREATE TABLE `gestaoalunos`.`alunos` (
-	`idaluno` INT NOT NULL AUTO_INCREMENT,
-	`nomealuno` VARCHAR(40) NOT NULL,
-	`numaluno` VARCHAR(15) NOT NULL, 
-	`altnumaluno` VARCHAR(15),
-	PRIMARY KEY (`idaluno`));
+/*
+	CREATE TABLE autor(
+		`idautor` INT NOT NULL AUTO_INCREMENT,
+        `nomeautor` VARCHAR(15) NOT NULL,
+        `sobrenome` VARCHAR(30) NULL,
+        PRIMARY KEY (`idautor`));
+    ------------------------------
+    DROP TABLE autor -> Para excluir
+*/
+	
+    CREATE TABLE editora (
+		`ideditora` INT NOT NULL AUTO_INCREMENT,
+        `nomeeditora` VARCHAR(30) NOT NULL,
+        PRIMARY KEY (`ideditora`)
+    );
     
-	DROP TABLE alunos; -> Excluir tabelas da tua BD
-*/
-
-/* 03 -> 
-
-*/
+    CREATE TABLE livro (
+		`idlivro` INT NOT NULL AUTO_INCREMENT,
+        `nomelivro` VARCHAR(30) NOT NULL,
+        `autor` INT NOT NULL,
+        `editora` INT NOT NULL,
+        PRIMARY KEY (`idlivro`),
+        INDEX `fkautor_idx` (`autor` ASC) VISIBLE,
+        INDEX `fkeditora_idx` (`editora`ASC) VISIBLE,
+        CONSTRAINT `fkautorXlivro`
+			FOREIGN KEY (`autor`)
+            REFERENCES `livraria`.`autor` (`idautor`)
+            ON DELETE RESTRICT
+            ON UPDATE RESTRICT,
+		CONSTRAINT `fkeditora`
+			FOREIGN KEY (`editora`)
+            REFERENCES `livraria`.`editora`(`ideditora`)
+			ON DELETE RESTRICT
+            ON UPDATE RESTRICT
+    );
